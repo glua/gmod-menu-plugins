b95f1f32685b0742c1859c03786721e5cfbc34df
LennyPenny <l.bernhardt@live.de>
2014-08-19 22:39:15 +0200

    switch to compilefile

--- lua/menu_plugins/init.lua
+++ lua/menu_plugins/init.lua
@@ -4,8 +4,7 @@ MsgC(Color(0, 0, 255), "\tloading...\n")
 for k, fil in pairs(file.Find("lua/menu_plugins/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
-	local path = "lua/menu_plugins/"..fil
-	RunStringEx(file.Read(path, "GAME"), path)
+	CompileFile("lua/menu_plugins/"..fil)
 end
 
 MsgC(Color(0, 255, 00), "All menu plgins loaded!\n")
