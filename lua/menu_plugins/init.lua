44ebf79d7628ffdc8064e12168e4b41f37cd5769
LennyPenny <l.bernhardt@live.de>
2014-08-19 22:40:50 +0200

    fix path

--- lua/menu_plugins/init.lua
+++ lua/menu_plugins/init.lua
@@ -4,7 +4,7 @@ MsgC(Color(0, 0, 255), "\tloading...\n")
 for k, fil in pairs(file.Find("lua/menu_plugins/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
-	CompileFile("lua/menu_plugins/"..fil)
+	CompileFile("menu_plugins/"..fil)
 end
 
 MsgC(Color(0, 255, 00), "All menu plgins loaded!\n")
