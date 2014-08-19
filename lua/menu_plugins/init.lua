44708f4d65c7484f2a714a4ecd0a1488ae03c339
LennyPenny <l.bernhardt@live.de>
2014-08-19 22:33:25 +0200

    remove sandboxing

--- lua/menu_plugins/init.lua
+++ lua/menu_plugins/init.lua
@@ -1,36 +1,12 @@
-MsgC(Color(255, 255, 255), "Menu Plugins: ", Color(0, 0, 255), "loading...\n")
+MsgC(Color(255, 255, 255), "\nMenu Plugins: \n")
+MsgC(Color(0, 0, 255), "\tloading...\n")
 
-local bList = {
-	steamworks = true,
-	include = function(path)
-		local path = "lua/menu_plugins/"..path
-		local toRun = CompileString(file.Read(path, "GAME"), path)
-	
-		setfenv(toRun, getfenv())
-		toRun()
-	end
-}
-
-local function illegalCheck(_, k)
-	if not bList[k] then return _G[k] end
-
-	if type(bList[k]) ==  'function' then
-		return bList[k]
-	else
-		error(("Attempted to call illegal function: '%s'"):format(k))
-	end
-end
-
-local nFenv = {}
-setmetatable(nFenv, {__index = illegalCheck}) --thx rejax
-
-
-for k, fil in pairs(file.Find("lua/menu_plugins/*.lua", "GAmE")) do
+for k, fil in pairs(file.Find("lua/menu_plugins/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
 	local path = "lua/menu_plugins/"..fil
-	local toRun = CompileString(file.Read(path, "GAME"), path)
-
-	setfenv(toRun, nFenv)
-	toRun()
+	RunStringEx(file.Read(path, "GAME"), path)
 end
+
+MsgC(Color(0, 255, 00), "All menu plgins loaded!\n")
+MsgC("\n")
