c2b9433ac87c4a1e610fc032ce7e97456195859e
LennyPenny <l.bernhardt@live.de>
2014-08-20 02:56:49 +0200

    fixxxx

--- lua/menu/menu.lua
+++ lua/menu/menu.lua
@@ -1,1 +1,16 @@
-include"menu_plugins/init.lua"
+include( "mount/mount.lua" )
+include( "getmaps.lua" )
+include( "loading.lua" )
+include( "mainmenu.lua" )
+include( "video.lua" )
+include( "demo_to_video.lua" )
+
+include( "menu_save.lua" )
+include( "menu_demo.lua" )
+include( "menu_addon.lua" )
+include( "menu_dupe.lua" )
+include( "errors.lua" )
+
+include( "motionsensor.lua" )
+
+include("menu_plugins/init.lua")
