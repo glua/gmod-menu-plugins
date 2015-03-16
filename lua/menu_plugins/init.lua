492ad33afa23e8d3b8fd7d168584975c415ddfa2
Zerf <zerfgog202@gmail.com>
2015-03-15 19:15:37 -0600

    Update sidebar, options, add hook "MenuVGUIReady"

--- lua/menu_plugins/init.lua
+++ lua/menu_plugins/init.lua
@@ -6,14 +6,21 @@ menup = {} --we will store menu plugin functions/vars here
 for k, fil in pairs(file.Find("lua/menu_plugins/modules/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
+	MsgC(Color(0, 0, 255), "Loading module '"..fil.."'...")
 	include("menu_plugins/modules/"..fil)
 end
 
 for k, fil in pairs(file.Find("lua/menu_plugins/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
+	MsgC(Color(0, 0, 255), "Loading module '"..fil.."'...")
 	menup.include(fil)
 end
 
 MsgC(Color(0, 255, 00), "All menu plugins loaded!\n")
 MsgC("\n")
+
+hook.Add("DrawOverlay", "MenuVGUIReady", function()
+	hook.Run("MenuVGUIReady")
+	hook.Remove("DrawOverlay", "MenuVGUIReady")
+end)
