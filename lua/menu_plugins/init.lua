95582bd7087e6de004f05cfe8fa3fc0a2bc510ec
Xavier Bergeron <xavbergeron@gmail.com>
2015-09-01 20:50:50 -0400

    Decrappify init output

--- lua/menu_plugins/init.lua
+++ lua/menu_plugins/init.lua
@@ -1,23 +1,30 @@
-MsgC(Color(255, 255, 255), "\nMenu Plugins: \n")
-MsgC(Color(0, 0, 255), "\tloading...\n")
+local Chartreuse = Color(127, 255, 0,   255)
+local Aquamarine = Color(127, 255, 212, 255)
+local LightBlue  = Color(72,  209, 204, 255)
+MsgC(LightBlue, "\n.-~= Menu Plugins =~-.\n")
 
 menup = {} --we will store menu plugin functions/vars here
 
 for k, fil in pairs(file.Find("lua/menu_plugins/modules/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
-	MsgC(Color(0, 0, 255), "Loading module '"..fil.."'...")
+	MsgC(Aquamarine, "Loading module '")
+	MsgC(Chartreuse, fil)
+	MsgC(Aquamarine, "'...\n")
 	include("menu_plugins/modules/"..fil)
 end
 
 for k, fil in pairs(file.Find("lua/menu_plugins/*.lua", "GAME")) do
 	if fil == "init.lua" then continue end
 
-	MsgC(Color(0, 0, 255), "Loading module '"..fil.."'...")
+	MsgC(Aquamarine, "Loading module '")
+	MsgC(Chartreuse, fil)
+	MsgC(Aquamarine, "'...\n")
+	
 	menup.include(fil)
 end
 
-MsgC(Color(0, 255, 00), "All menu plugins loaded!\n")
+MsgC(LightBlue, "All menu plugins loaded!\n")
 MsgC("\n")
 
 hook.Add("DrawOverlay", "MenuVGUIReady", function()
